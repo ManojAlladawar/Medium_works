cf6f58dbca0db095e0276bc8f19b4c4881ca9f52
Manoj Alladawar <89651768+ManojAlladawar@users.noreply.github.com>
2022-09-24 18:48:47 +0530

    Create Last.py

---
+++ Last.py
@@ -0,0 +1,35 @@
+import requests
+
+
+class Browser:
+
+  def __init__(self):
+    self.session = requests.session()
+
+  def get(self, url, headers=None, verbose=0):
+    headers = headers or {}
+    if verbose >= 2:
+      print(f"Browser GET Request: {url} (headers={headers})")
+      
+    response = self.session.get(url, headers=headers)
+    
+    if verbose >= 1:
+      print((
+        f"Browser GET Response: {url} (headers={headers})"
+        f" Status Code {response.status_code} Content {response.content}"
+      ))
+    return response
+    
+  def post(self, url, data=None, headers=None, verbose=0):
+    headers = headers or {}
+    if verbose >= 2:
+      print(f"Browser POST Request: {url} (headers={headers} data={data})")
+      
+    response = self.session.post(url, data=data, headers=headers)
+    
+    if verbose >= 1:
+      print((
+        f"Browser POST Response: {url} (headers={headers} data={data})"
+        f" Status Code {response.status_code} Content {response.content}"
+      ))
+    return response
